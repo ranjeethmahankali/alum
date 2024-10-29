83bd40f527295c8e27e26cefe0630ceb19fb49d5
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-10-29 10:38:02 -0400

    Separate out into locked and lock free versions of vector functions

--- src/vector.rs
+++ src/vector.rs
@@ -85,7 +85,7 @@ impl TVec3 for glam::Vec3 {
     }
 }
 
-impl<VecT: TVec3> PolyMeshT<VecT>
+impl<VecT> PolyMeshT<VecT>
 where
     VecT::Scalar: TScalar
         + TPropData
@@ -93,14 +93,16 @@ where
         + Add<Output = VecT::Scalar>
         + PartialOrd
         + Div<Output = VecT::Scalar>,
-    VecT: Add<Output = VecT> + Sub<Output = VecT> + Div<VecT::Scalar, Output = VecT>,
+    VecT: TVec3 + Add<Output = VecT> + Sub<Output = VecT> + Div<VecT::Scalar, Output = VecT>,
 {
-    pub fn calc_face_normal(&self, f: FH) -> Result<VecT, Error> {
+    pub fn calc_face_normal_locked(&self, f: FH) -> Result<VecT, Error> {
+        let points = self.points().try_borrow()?;
+        Ok(self.calc_face_normal(f, &points))
+    }
+
+    pub fn calc_face_normal(&self, f: FH, points: &[VecT]) -> VecT {
         // Use newell's method to compute the normal.
         let (nverts, x, y, z) = {
-            // Borrow points inside the scope to limit their lifetime.
-            let points = self.points().try_borrow()?;
-            let points: &[VecT] = &points;
             iterator::fh_ccw_iter(self.topology(), f).fold(
                 (
                     0usize,
@@ -125,18 +127,21 @@ where
         };
         if nverts < 3 {
             // Guard against degenerate cases.
-            return Ok(VecT::new(
+            return VecT::new(
                 VecT::Scalar::from_f64(0.0),
                 VecT::Scalar::from_f64(0.0),
                 VecT::Scalar::from_f64(0.0),
-            ));
+            );
         }
-        Ok(VecT::new(x, y, z).normalized())
+        VecT::new(x, y, z).normalized()
     }
 
-    pub fn calc_face_centroid(&self, f: FH) -> Result<VecT, Error> {
+    pub fn calc_face_centroid_locked(&self, f: FH) -> Result<VecT, Error> {
         let points = self.points().try_borrow()?;
-        let points: &[VecT] = &points;
+        Ok(self.calc_face_centroid(f, &points))
+    }
+
+    pub fn calc_face_centroid(&self, f: FH, points: &[VecT]) -> VecT {
         let (denom, total) = iterator::fv_ccw_iter(self.topology(), f).fold(
             (
                 VecT::Scalar::from_f64(0.0),
@@ -153,14 +158,16 @@ where
                 )
             },
         );
-        Ok(total / denom)
+        total / denom
     }
 
-    pub fn calc_halfedge_vector(&self, h: HH) -> Result<VecT, Error> {
+    pub fn calc_halfedge_vector_locked(&self, h: HH) -> Result<VecT, Error> {
         let points = self.points().try_borrow()?;
-        let points: &[VecT] = &points;
-        Ok(points[self.to_vertex(h).index() as usize]
-            - points[self.from_vertex(h).index() as usize])
+        Ok(self.calc_halfedge_vector(h, &points))
+    }
+
+    pub fn calc_halfedge_vector(&self, h: HH, points: &[VecT]) -> VecT {
+        points[self.to_vertex(h).index() as usize] - points[self.from_vertex(h).index() as usize]
     }
 }
 
@@ -175,7 +182,7 @@ mod test {
         assert_eq!(
             qbox.faces()
                 .map(|f| {
-                    qbox.calc_face_normal(f)
+                    qbox.calc_face_normal_locked(f)
                         .expect("Cannot compute face centroid")
                 })
                 .collect::<Vec<_>>(),
@@ -197,7 +204,7 @@ mod test {
         assert_eq!(
             qbox.faces()
                 .map(|f| {
-                    qbox.calc_face_centroid(f)
+                    qbox.calc_face_centroid_locked(f)
                         .expect("Cannot compute face centroid")
                 })
                 .collect::<Vec<_>>(),
@@ -219,7 +226,7 @@ mod test {
         assert_eq!(
             qbox.halfedges()
                 .map(|h| {
-                    qbox.calc_halfedge_vector(h)
+                    qbox.calc_halfedge_vector_locked(h)
                         .expect("Cannot compute halfedge vector")
                 })
                 .collect::<Vec<_>>(),
