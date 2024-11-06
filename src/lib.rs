5c695efb256bc3fd2e7c513afbcb3cbcb61106a3
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-11-06 11:25:31 -0500

    Clean up the property API

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ pub mod error;
 pub mod mesh;
 pub mod obj;
 pub mod primitive;
-pub mod property;
 pub mod status;
 pub mod vector;
 
 mod iterator;
+mod property;
 mod topol;
 
 pub type PolyMeshF32 = mesh::PolyMeshF32;
