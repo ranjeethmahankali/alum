443ba8af5758975d5c651b488d47066da50810aa
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-11-13 19:30:59 -0500

    Fix clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -6,41 +6,41 @@ crate has an API that is very similar to OpenMesh.
 # Overview
 
 + A halfedge datastructure is used to represent the topology of a mesh, i.e. the
-connectivity of vertices, edges and faces.
+  connectivity of vertices, edges and faces.
 
 + The generic polygon mesh type [`PolyMeshT<DIM, A>`] can be used with custom
-geometric types. These include:
+  geometric types. These include:
 
   + Vector/Point type used to represent the positions of vertices, normals of
-faces etc.
+    faces etc.
 
   + The scalar type used to represent the areas of faces, lengths of edges, and
-angles between edges etc.
+    angles between edges etc.
 
   + Number of spacial dimensions the mesh lives in. Often this is 2 or 3.
 
 + To use the generic [`PolyMeshT<DIM, A>`] with your own geometric types, you
-must provide an implementation of [`Adaptor`] that tells this crate how to work
-with your geometric types. A basic implementation of the [`Adaptor`] is needed
-to use [`PolyMeshT<DIM, A>`]. There are ancilliary adaptor types that can be
-optionally implemented to enable variuos geometric features of [`PolyMeshT<DIM,
-A>`] such as computing areas of faces, normals of faces, normals of vertices,
-dihedral angles etc.
+  must provide an implementation of [`Adaptor`] that tells this crate how to
+  work with your geometric types. A basic implementation of the [`Adaptor`] is
+  needed to use [`PolyMeshT<DIM, A>`]. There are ancilliary adaptor types that
+  can be optionally implemented to enable variuos geometric features of
+  [`PolyMeshT<DIM, A>`] such as computing areas of faces, normals of faces,
+  normals of vertices, dihedral angles etc.
 
 + Optionally, this crate provides some builtin adaptor implementations, and
-concrete mesh types that can be used without any boilerplate. These builtin
-geometric types use the [`glam`](https://crates.io/crates/glam) crate and can be
-found in the [`alum_glam`] module. `use_glam` feature is required by
-these. These builtin types include:
+  concrete mesh types that can be used without any boilerplate. These builtin
+  geometric types use the [`glam`](https://crates.io/crates/glam) crate and can
+  be found in the [`alum_glam`] module. `use_glam` feature is required by
+  these. These builtin types include:
 
   + [`PolyMeshF32`](alum_glam::PolyMeshF32) using the
-  [`BuiltInAdaptorF32`](alum_glam::BuiltInAdaptorF32). This mesh uses 32 bit
-  floating point types to represent the geometry.
+    [`BuiltInAdaptorF32`](alum_glam::BuiltInAdaptorF32). This mesh uses 32 bit
+    floating point types to represent the geometry.
 
   + [`PolyMeshF64`](alum_glam::PolyMeshF64) using the
-  [`BuiltInAdaptorF64`](alum_glam::BuiltInAdaptorF64). This mesh uses 64 bit
-  floating point types to represent the geometry.
- */
+    [`BuiltInAdaptorF64`](alum_glam::BuiltInAdaptorF64). This mesh uses 64 bit
+    floating point types to represent the geometry.
+*/
 
 mod edit;
 mod element;
